6ce894972afc8d88fcb838c1b9ecd5e4127a3ba4
somecallmejosh <josh@thebrileys.com>
2024-11-15 09:38:57 -0500

    Add hover effect to index cards.

--- pages/index.vue
+++ pages/index.vue
@@ -24,11 +24,16 @@
               :delay="100 * index"
               class="mb-4 transition-all duration-300 opacity-0 lg:mb-10 hover:shadow-2xl">
               <NuxtLink :to="`/gallery/${artwork.id}`" class="relative block w-full overflow-hidden mix-blend-overlay group">
-                <NuxtImg :src="artwork.images.thumbnail" width="310" class="relative z-0 w-full transition-all duration-500 group-hover:scale-110 group-hover:translate-y-4" :alt="artwork.name" />
-                <div class="absolute inset-0 z-10 transition-opacity duration-300 bg-gradient-to-t from-black/90 via-black/40 to-transparent group-hover:opacity-0"></div>
-                <div class="absolute bottom-0 left-0 z-20 max-w-full p-4 lg:p-8 space-y-0.5 lg:space-y-2 text-left text-white">
-                  <h2 class="font-bold lg:text-2xl">{{ artwork.name }}</h2>
-                  <p class="text-xs truncate transition-opacity duration-200 opacity-75 lg:text-sm group-hover:opacity-100">{{ artwork.artist.name }}</p>
+                <NuxtImg :src="artwork.images.thumbnail" width="310" class="relative z-0 w-full transition-all duration-500 scale-[120%] group-hover:scale-100" :alt="artwork.name" />
+                <div class="absolute inset-0 transition-all duration-500 translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100">
+                  <div class="absolute inset-0 z-10 bg-gradient-to-t from-black/90 via-black/40 to-transparent"></div>
+                  <div class="absolute bottom-0 left-0 z-20 max-w-full p-4 lg:p-8 space-y-0.5 lg:space-y-2 text-left text-white">
+                    <h2 class="font-bold lg:text-2xl">{{ artwork.name }}</h2>
+                    <div class="flex items-center">
+                      <p class="text-xs truncate transition-opacity duration-200 opacity-75 lg:text-sm group-hover:opacity-100">{{ artwork.artist.name }}</p>
+                      <icon-right-arrow class="block w-6 h-6 duration-300 delay-500 -translate-x-full opacity-0 group-hover:translate-x-full group-hover:opacity-100" />
+                    </div>
+                  </div>
                 </div>
               </NuxtLink>
             </div>
